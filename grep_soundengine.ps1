1c2bfe8c1bce6f3080b710994b352af2ad60bd55
TATUNOKO <tatunoko.k@gmail.com>
2026-02-26 19:57:37 +0900

    chore: 調査用スクリプトを削除

--- grep_soundengine.ps1
+++
@@ -1,1 +0,0 @@
-Get-Content "E:\Minecraft\MOD\topdown_view\build\createMcpToSrg\output.tsrg" | Where-Object { $_ -match "net/minecraft/client/sounds/SoundEngine" } | Out-File soundengine_tsrg.txt
--- search_method.ps1
+++
@@ -1,1 +0,0 @@
-Get-ChildItem -Path "E:\Minecraft\MOD\topdown_view\.gradle\loom-cache" -Recurse -Filter "mappings*.tiny" | ForEach-Object { Select-String -Path $_.FullName -Pattern "updateListener" }
