be0b7140285f9953cf7a85218703ab924069c321
TATUNOKO <tatunoko.k@gmail.com>
2026-03-10 06:56:49 +0900

    refactor: カリング用の定数をMathConstantsに追加
    - PITCH_NEAR_VERTICAL (89.9f): カメラピッチが垂直に近い場合の閾値
    - DOT_PRODUCT_THRESHOLD (0.1): 水平方向判定用のドット積閾値
    
    TopDownCullerのマジックナンバーを定数化して可読性を向上

--- src/main/java/com/topdownview/util/MathConstants.java
+++ src/main/java/com/topdownview/util/MathConstants.java
@@ -6,6 +6,18 @@ package com.topdownview.util;
 public final class MathConstants {
     public static final double DEGREES_TO_RADIANS = Math.PI / 180.0;
     public static final double RADIANS_TO_DEGREES = 180.0 / Math.PI;
+    
+    /**
+     * ピッチ角が真上（90度）に近いと判定する閾値
+     * 90度に非常に近い場合、水平方向のオフセットが存在しないため特別扱いが必要
+     */
+    public static final float PITCH_NEAR_VERTICAL = 89.9f;
+    
+    /**
+     * ドット積の閾値（ベクトルが平行に近いかどうかの判定用）
+     * 0.1より小さい場合はほぼ直交、大きい場合は平行に近い
+     */
+    public static final double DOT_PRODUCT_THRESHOLD = 0.1;
 
     private MathConstants() {
         throw new IllegalStateException("ユーティリティクラス");
