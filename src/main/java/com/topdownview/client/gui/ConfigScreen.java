d9240b68ee0f29c940fec96823bf6042b59c1828
TATUNOKO <tatunoko.k@gmail.com>
2026-07-07 20:24:52 +0900

    Baritone未導入時に設定を非表示に

--- src/main/java/com/topdownview/client/gui/ConfigScreen.java
+++ src/main/java/com/topdownview/client/gui/ConfigScreen.java
@@ -1,6 +1,7 @@
 package com.topdownview.client.gui;
 
 import com.topdownview.Config;
+import com.topdownview.baritone.BaritoneIntegration;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.components.AbstractSliderButton;
 import net.minecraft.client.gui.components.Button;
@@ -308,21 +309,23 @@ public class ConfigScreen extends Screen {
                 Config.getSprintDistanceThreshold(), 1.0, 50.0, val -> Config.setSprintDistanceThreshold(val)));
         y += sp;
 
-        y = addSection(y, "topdown_view.config.section.baritone", tx);
-        addRightWidget(Button
-                .builder(getOnOffComponent("topdown_view.config.baritone_render_path", Config.isBaritoneRenderPath()), btn -> {
-                    Config.setBaritoneRenderPath(!Config.isBaritoneRenderPath());
-                    btn.setMessage(getOnOffComponent("topdown_view.config.baritone_render_path", Config.isBaritoneRenderPath()));
-                }).bounds(x, y, w, h)
-                .tooltip(Tooltip.create(Component.translatable("topdown_view.config.baritone_render_path.tooltip"))).build());
-        y += sp;
-        addRightWidget(Button
-                .builder(getOnOffComponent("topdown_view.config.baritone_render_goal", Config.isBaritoneRenderGoal()), btn -> {
-                    Config.setBaritoneRenderGoal(!Config.isBaritoneRenderGoal());
-                    btn.setMessage(getOnOffComponent("topdown_view.config.baritone_render_goal", Config.isBaritoneRenderGoal()));
-                }).bounds(x, y, w, h)
-                .tooltip(Tooltip.create(Component.translatable("topdown_view.config.baritone_render_goal.tooltip"))).build());
-        y += sp;
+        if (BaritoneIntegration.isBaritoneAvailable()) {
+            y = addSection(y, "topdown_view.config.section.baritone", tx);
+            addRightWidget(Button
+                    .builder(getOnOffComponent("topdown_view.config.baritone_render_path", Config.isBaritoneRenderPath()), btn -> {
+                        Config.setBaritoneRenderPath(!Config.isBaritoneRenderPath());
+                        btn.setMessage(getOnOffComponent("topdown_view.config.baritone_render_path", Config.isBaritoneRenderPath()));
+                    }).bounds(x, y, w, h)
+                    .tooltip(Tooltip.create(Component.translatable("topdown_view.config.baritone_render_path.tooltip"))).build());
+            y += sp;
+            addRightWidget(Button
+                    .builder(getOnOffComponent("topdown_view.config.baritone_render_goal", Config.isBaritoneRenderGoal()), btn -> {
+                        Config.setBaritoneRenderGoal(!Config.isBaritoneRenderGoal());
+                        btn.setMessage(getOnOffComponent("topdown_view.config.baritone_render_goal", Config.isBaritoneRenderGoal()));
+                    }).bounds(x, y, w, h)
+                    .tooltip(Tooltip.create(Component.translatable("topdown_view.config.baritone_render_goal.tooltip"))).build());
+            y += sp;
+        }
 
         y = addSection(y, "topdown_view.config.section.auto_jump", tx);
         addRightWidget(
